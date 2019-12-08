86f27fb33b3d8350b78a83f10e5d770c36d3bf35
mapleFU <1506118561@qq.com>
2019-12-08 21:33:01 +0800

    [ADD] adding testing for traversing

--- benches/cache_benchmark.rs
+++ benches/cache_benchmark.rs
@@ -1,16 +1,39 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
+/// time:   [340.70 us 341.18 us 341.77 us]
+fn traverse_array1(sample: &[[i32; 1024]; 1024]) {
+    for i in 0..1024 {
+        for j in 0..1024 {
+            black_box(sample[i][j]);
+        }
     }
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+/// time:   [5.5622 ms 5.5658 ms 5.5702 ms]
+fn traverse_array2(sample: &[[i32; 1024]; 1024]) {
+    for i in 0..1024 {
+        for j in 0..1024 {
+            black_box(sample[j][i]);
+        }
+    }
 }
 
-criterion_group!(benches, criterion_benchmark);
+/// This part test about "cache-friendly-code".
+fn bench_array(c: &mut Criterion) {
+    let mut group = c.benchmark_group("traverse-array");
+    let sample_array = [[0; 1024]; 1024];
+    group.bench_with_input(
+        BenchmarkId::new("x-y-traverse", 1i32),
+        &sample_array,
+        |b, sample_array| b.iter(|| traverse_array1(&sample_array)),
+    );
+    group.bench_with_input(
+        BenchmarkId::new("y-x-traverse", 2i32),
+        &sample_array,
+        |b, sample_array| b.iter(|| traverse_array2(&sample_array)),
+    );
+    group.finish();
+}
+
+criterion_group!(benches, bench_array);
 criterion_main!(benches);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+fn traverse_array1() {
+    let mut sample: [[i32; 1024]; 1024] = [[0; 1024]; 1024];
+    for i in 0..1024 {
+        for j in 0..1024 {
+            sample[i][j] += 1;
+        }
+    }
+}
+
+fn traverse_array2() {
+    let mut sample: [[i32; 1024]; 1024] = [[0; 1024]; 1024];
+    for i in 0..1024 {
+        for j in 0..1024 {
+            sample[j][i] += 1;
+        }
+    }
+}
+
 fn main() {
+    traverse_array1();
+    traverse_array2();
     println!("Hello, cache!");
 }
